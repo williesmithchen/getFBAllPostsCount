5689e1a7da61cda958836123e49ac1090c0793df
williesmithchen <williesmithchen@gmail.com>
2015-10-16 19:16:01 +0800

    remove data

--- react.js
+++ react.js
@@ -52,7 +52,6 @@ var CommentItemType = React.createClass({
 
 var Comment = React.createClass({
   render: function() {
-    var data = { this.props.data };
     return (
       <CommentBox { ...data } />
     );
